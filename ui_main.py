92e9dbc885dcf646718aa40f8ab89da0448e9fd6
laxitlal <laxitlal143@gmail.com>
2023-10-26 04:05:10 +0530

    user.py added, seperate function for server connect, add refresh button

--- ui_main.py
+++ ui_main.py
@@ -1,10 +1,11 @@
 from interface import Ui_MainWindow
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from users import active_users
 import socket
 import json
 
-class DictionaryTableWidget(QTableWidget):
+'''class DictionaryTableWidget(QTableWidget):
     def __init__(self, data_dict):
         super().__init__()
         self.data_dict = data_dict
@@ -31,7 +32,33 @@ class active_users(QMainWindow):
         self.table_widget = DictionaryTableWidget(data_dict)
         layout.addWidget(self.table_widget)
         widget.setLayout(layout)
-        self.setCentralWidget(widget)
+        self.setCentralWidget(widget)'''
+
+class second_window(QMainWindow):
+    def __init__(self,data_dict):
+        super().__init__()
+        self.data_dict = data_dict
+        self.ui = active_users(data_dict)
+        self.ui.setupUi(self)
+        self.ui.reset.clicked.connect(self.refresh_op)
+
+    def refresh_op(self):
+        data_dict = server_connection()
+        self.close()
+        self.user_interface  = second_window(data_dict)
+        self.user_interface.show()
+
+def server_connection(msg=""):
+    server_ip = "10.81.40.15"
+    server_port = 5555
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client_socket.connect((server_ip,server_port))
+    if len(msg) > 0:
+        client_socket.send(msg.encode('utf-8'))
+    data_str = client_socket.recv(1024).decode('utf-8')
+    data_dict = json.loads(data_str)
+    client_socket.close()
+    return data_dict
 
 class MyForm(QMainWindow):
     def __init__(self):
@@ -43,20 +70,14 @@ class MyForm(QMainWindow):
     def submit_op(self):
         #print(self.ui.username.text())
         #send username to server (hardcode server ip) and receive a dictionary
-        server_ip = "10.81.40.15"
-        server_port = 5555
-        client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        client_socket.connect((server_ip,server_port))
         msg =self.ui.username.text()
-        client_socket.send(msg.encode('utf-8'))
-        data_str = client_socket.recv(1024).decode('utf-8')
-        data_dict = json.loads(data_str)
-        print(data_dict)
-        client_socket.close()
+        data_dict = server_connection(msg)
         #dictionary is opened in new ui in form of table
         self.close()
-        self.users_interface = active_users(data_dict)
-        self.users_interface.show()
+        self.user_interface  = second_window(data_dict)
+        self.user_interface.show()
+        '''self.users_interface = active_users(data_dict)
+        self.users_interface.show()'''
         #click on the username, new ui of file selection opens, select file and transfer by clicking "transfer"
 
 if __name__ == "__main__":
